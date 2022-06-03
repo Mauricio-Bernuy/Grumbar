800bfbc1aa0f4768af46b73b1ec1111ed9595177
1v1menub <ignacio.rubio@utec.edu.pe>
2022-06-03 04:50:00 -0500

    armar circulitos circules

--- frontend/src/components/objects/FabricRoom.js
+++ frontend/src/components/objects/FabricRoom.js
@@ -14,34 +14,88 @@ const FabricRoom = () => {
     // const addTextBox = (e) => {
     //     document.getElementById("fabric-asset-upload").click()
     // }
+
+
+  var coords = []
+  var puntitos = []
+
   let lock = false;
-  const addAsset = (e) => {
+  const addRoom = (e) => {
 	  canvas.on('mouse:up', function(opt) {
-		let x, y, x2, y2;
 		let pointer = canvas.getPointer(opt.e);
-		x = pointer.x;
-		y = pointer.y;
-		x2 = x + 200;
-		y2 = y + 300;
-		let line = new fabric.Line([x, y, x2, y2]);
-		line.stroke = '#FFFFF';
-		canvas.add(line);
 
-		let listener = canvas.__eventListeners['mouse:up'];
-		let curr = listener[listener.length - 1];
+		for(let i = 0; i < coords.length; i++) {
+			console.log(coords[i].x)
+			console.log(coords[i].y)
+        }
 
-		console.log("listen: ", listener);
-		console.log("curr: ", curr);
-		canvas.off('mouse:up', curr);
+		
+		var object = new fabric.Circle({
+			radius: 5,
+			fill: 'blue',
+			left: pointer.x-5,
+			top: pointer.y-5,
+			selectable: false,
+			hasControls: false,
+			lockMovementX: true,
+			ockMovementY: true
+			
+		});
 
-		lock = false;
+		puntitos.push(object)
+		canvas.add(object); 
+
+		if(coords.length == 0) {
+			var objectPatrol = new fabric.Circle({
+				radius: 20,
+				left: pointer.x-20,
+				top: pointer.y-20,
+				fill: 'rgba(0,0,0,0)',
+				hasBorder: true,
+				stroke: 'black',
+				strokeWidth: 2,
+				selectable: false,
+				hasControls: false,
+				lockMovementX: true,
+				ockMovementY: true
+			});
+
+			puntitos.push(objectPatrol)
+			canvas.add(objectPatrol);
+		}
+		
+		coords.push(pointer)
+
+		if (coords.length > 1){
+            console.log("length more than zero");
+            if (coords[0].x -20 <= pointer.x && pointer.x <= coords[0].x +20){
+                if (coords[0].y -20 <= pointer.y && pointer.y <= coords[0].y +20){
+                    console.log("poly closed");
+                    let listener = canvas.__eventListeners['mouse:up'];
+                    let curr = listener[listener.length - 1];
+
+                    console.log("listen: ", listener);
+                    console.log("curr: ", curr);
+                    canvas.off('mouse:up', curr);
+
+                    lock = false;
+
+					for(let i = 0; i < puntitos.length; i++) {
+						canvas.remove(puntitos[i])
+					}
+
+                    coords = [];
+					puntitos = [];
+                }
+            }
+        }
+
       });
-
   };
 
   const bellido = (e) => {
 	if (lock === false) {
-	  addAsset(e);
+	  addRoom(e);
 	  lock = true;
 	}else
 	  console.log("OH MY! THIS LOCK BE LIKE: 🥵🍆💦");
