13b84163bbc71a6ba29bdba560a8fb1f3b8257ad
Munay-k <gonzalo.alfacaso@gmail.com>
2022-06-03 01:58:46 -0500

    onClick and lock implementation

--- frontend/src/components/objects/FabricAsset.js
+++ frontend/src/components/objects/FabricAsset.js
@@ -14,19 +14,41 @@ const FabricAsset = () => {
     // const addTextBox = (e) => {
     //     document.getElementById("fabric-asset-upload").click()
     // }
-    const addAsset = (e) => {
+  let lock = false;
+  const addAsset = (e) => {
+	  canvas.on('mouse:up', function(opt) {
         const file = testasset;
         fabric.Image.fromURL(file, function(img) {
             img.scaleToWidth(100);
             img.snapAngle=15
+		  //img.top = (opt.e.clientY - fabric.util.invertTransform(canvas.viewportTransform)[5]) * canvas.getZoom();
+		  //img.left = (opt.e.clientX - fabric.util.invertTransform(canvas.viewportTransform)[4]) * canvas.getZoom();
+			img.top = opt.e.layerY;
+			img.left = opt.e.layerX;
             canvas.add(img);
         });
-    };
+		let listener = canvas.__eventListeners['mouse:up'];
+		let curr = listener[listener.length - 1];
 
-    
+		console.log("listen: ", listener);
+		console.log("curr: ", curr);
+		canvas.off('mouse:up', curr);
+
+		lock = false;
+	  })
+  };
+
+  const bellido = (e) => {
+	if (lock === false) {
+	  addAsset(e);
+	  lock = true;
+	}else
+	  console.log("OH MY THIS LOCK BE LIKE: 🥵🍆💦");
+  };
+
     return (
         <>
-            <button onClick={addAsset}>Add Asset</button>
+            <button onClick={bellido}>Add Asset</button>
             {/* <input type="file" id="fabric-asset" accept="image/*" onChange={onImageUpload}
                    style={{ display: "none" }}/> */}
         </>
