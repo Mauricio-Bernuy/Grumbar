75cbbcacef2f11def1c7647d00634e0143f9fccc
Mauricio Bernuy <mauricio.bernuy@utec.edu.pe>
2022-06-29 21:51:19 -0500

    Login Design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,65 +1,78 @@
-import React, { useEffect }      from 'react';
-import FabricCanvas              from "./components/FabricCanvas"
-import FabricToolbar             from "./components/FabricToolbar"
-import { FabricContextProvider } from "./context/FabricContext"
-import {NavBar} from "./components/NavBar"
-import {NavItem} from "./components/NavItem"
-import {DropdownMenu} from "./components/DropdownMenu"
-import { LoginButton } from "./components/Login"
-import {ReactComponent as ProfileIcon} from "./profileicon.svg"
-import {useAuth0} from "@auth0/auth0-react"
-import {FpsView} from "react-fps";
+import React, { useEffect } from "react";
+import FabricCanvas from "./components/FabricCanvas";
+import FabricToolbar from "./components/FabricToolbar";
+import { FabricContextProvider } from "./context/FabricContext";
+import { NavBar } from "./components/NavBar";
+import { NavItem } from "./components/NavItem";
+import { DropdownMenu } from "./components/DropdownMenu";
+import { LoginButton } from "./components/Login";
+import { ReactComponent as ProfileIcon } from "./profileicon.svg";
+import { useAuth0 } from "@auth0/auth0-react";
+import { FpsView } from "react-fps";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Fade } from "@mui/material";
+import { TransitionGroup } from "react-transition-group";
 
 function App() {
-  const {user,isAuthenticated} = useAuth0()
+  const { user, isAuthenticated } = useAuth0();
 
-    useEffect(() => {
-        document.body.style.overflow = "hidden";
-      }, []);
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+  }, []);
 
-    return (
-      <>
-        <div style={{"padding": "0px 0px", 'height':'100vh' }}>
-          {isAuthenticated ? (
-            <> 
-              <NavBar>
-                <NavItem icon={<ProfileIcon/>}>
-                  <DropdownMenu/>
-                </NavItem>
-              </NavBar>
-              <FabricContextProvider>
-                <div style={{ "display": "flex", "alignItems": "stretch" }}>
-                    {/* <div style={{ "width": "100px", "background": "gray", "padding": "20px 20px 0 20px" }}> */}
-                    <div>
+  return (
+    <>
+      <div style={{ padding: "0px 0px", height: "100vh" }}>
+        {/* <LoginButton /> */}
+        {isAuthenticated ? (
+          <>
+            <NavBar>
+              <NavItem icon={<ProfileIcon />}>
+                <DropdownMenu />
+              </NavItem>
+            </NavBar>
+            <FabricContextProvider>
+              <div style={{ display: "flex", alignItems: "stretch" }}>
+                <div>
+                  <FabricToolbar />
 
-                      <FabricToolbar/>
-                      
-                      <FpsView width={100} height={20} top={window.innerHeight-50} left={0}/>
-                    </div>
-                    <div style={{ "flex": "1" }}>
-                      <FabricCanvas/>
-                    </div>
+                  <FpsView
+                    width={100}
+                    height={20}
+                    top={window.innerHeight - 50}
+                    left={0}
+                  />
                 </div>
-              </FabricContextProvider>
-              
-            </>
-          )
-          :
-          (
-            <>
-              <LoginButton />
-            </>
-          )}
-
-        </div>
-      </>
-    );
+                <Fade
+                  in={true}
+                  style={{ transitionDelay: "500ms" }}
+                  timeout={1000}
+                >
+                  <div style={{ flex: "1" }}>
+                    <FabricCanvas />
+                  </div>
+                </Fade>
+              </div>
+            </FabricContextProvider>
+          </>
+        ) : (
+          <>
+            <TransitionGroup>
+              <Fade in={true} timeout={500}>
+                <div>
+                  <LoginButton />
+                </div>
+              </Fade>
+            </TransitionGroup>
+          </>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Icon.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import { useEffect } from "react";
+import React from "react";
+import { ReactComponent as Svg } from "../assets/GrumbarNoFill.svg";
+
+const AnimatedIcon = (props) => {
+  return (
+    <Svg fill={props.fill}/>
+  )
+}
+export default AnimatedIcon;
+
